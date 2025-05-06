5495ad4a721fa94841a78d5e9614398a235496aa
Felix Jungbluth <fjungbluth@ethz.ch>
2025-05-06 06:50:25 +0200

    Fix some settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,16 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), svgr()],
+    build: {
+        chunkSizeWarningLimit: 1000, // optional: raise warning threshold
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.split('node_modules/')[1].split('/')[0]            // splits each dependency into its own chunk
+                    }
+                }
+            }
+        }
+    }
 })
