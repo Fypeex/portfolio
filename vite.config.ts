9b110af3184c5f256d1a46cf962ae136c8609c25
Felix Jungbluth <fjungbluth@ethz.ch>
2025-05-06 22:41:34 +0200

    Add google and 404 redirect

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
-
+import {resolve} from 'path'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), svgr()],
     build: {
         chunkSizeWarningLimit: 1000, // optional: raise warning threshold
         rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                google: resolve(__dirname, 'public/google0860d9b864b3d0c2.html')
+            },
             output: {
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
